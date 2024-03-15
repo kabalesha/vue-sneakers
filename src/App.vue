228ca973fba34376fee9d24185930f90dcc1af68
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-03-15 21:42:12 +0100

    tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup></script>
+<script setup>
+import TestComponent from './components/TestComponent.vue'
+</script>
 
 <template>
   <h1>Test Vue :)</h1>
 </template>
-
-<style scoped></style>
