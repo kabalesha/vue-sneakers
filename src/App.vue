03d714df0a5b3e31c984856e036bf16785898424
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-03-16 23:58:58 +0100

    rule eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Header from './components/Header/Header.vue'
 import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
+    <Drawer />
     <Header />
 
     <div class="p-10">
---
+++ src/components/CartItem.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Element</h2>
+</template>
