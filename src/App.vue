2581007cf9cf194434fdde7b12714b2c11c92c2d
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-03-18 11:20:15 +0100

    input

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,29 @@ import Drawer from './components/Drawer.vue'
 
 <template>
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
-    <Drawer />
+    <!-- <Drawer /> -->
     <Header />
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-10">All staff</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">All staff</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option value="">By name</option>
+            <option value="">From low to high price</option>
+            <option value="">From high to low price</option>
+          </select>
+
+          <div class="relative">
+            <img src="/search.svg" class="absolute left-4 top-3" alt="" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
